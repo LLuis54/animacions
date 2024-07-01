13659c409465a9a14e407d1ef362eed12e98ad8a
LLuis54 <llorente@xtec.cat>
2024-07-01 16:22:44 +0200

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,114 @@
+input.onButtonPressed(Button.A, function () {
+    basic.pause(500)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # # .
+        . . # . .
+        . . # # .
+        . . . # .
+        . . # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        . # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
